ec0b59e1bfff7d13675f8d61db2007d183a948cf
Baris YILMAZ <baris.yilmaz.1985@gmail.com>
2014-04-10 17:48:31 +0300

    DropDownList is imlemented.

--- Scenes/LoginScene.lua
+++ Scenes/LoginScene.lua
@@ -8,6 +8,7 @@ local Utils        = require "libs.Util.Utils"
 local LoadingMask  = require "Views.LoadingMask"
 local BaseScene    = require "Scenes.BaseScene"
 local Logger       = require "libs.Log.Logger"
+local DropDownMenu = require "libs.DDM.DropDownMenu"
 
 local scene = BaseScene.new()
 
@@ -263,6 +264,17 @@ function scene:createScene( event )
     displayGroup:insert( loginButton )
     displayGroup.x = centerX
     displayGroup.y = centerY
+    
+    -- Test Environment
+    local dataList = { "ISTANBUL", "IZMIR", "ANKARA", "GOTHAM" }
+    local ddm = DropDownMenu.new{
+                                dataList = dataList,
+                                parent = displayGroup,
+                                buttonWidth = 360,
+                                buttonHeight = 40,
+                                x = 100,
+                                y = 1,
+                            } 
 end
 
 local superEnterScene = scene.enterScene
--- main.lua
+++ main.lua
@@ -4,10 +4,12 @@ local widget       = require "widget"
 local mime         = require "mime"
 local json         = require "json"
 local string       = require "string"
+local timer        = require "timer"
 local storyboard   = require "storyboard"
 local DataService  = require "Network.DataService"
 local Logger       = require "libs.Log.Logger"
 local native       = require "native"
+local DropDownMenu = require "libs.DDM.DropDownMenu"
 
 require "ParameterConfig"
 
@@ -26,6 +28,40 @@ Logger:debug("main", "general", "PixelWidth: " .. display.pixelWidth .. " PixelH
 Logger:debug("main", "general", "ContentWidth: " .. display.contentWidth .. " ContentHeight: " .. display.contentHeight)
 Logger:debug("main", "general", "ContentWidth: " .. display.contentWidth .. " ContentHeight: " .. display.contentHeight)
 
-storyboard.gotoScene( "Scenes.AppointmentScene", "slideLeft", 400)
+storyboard.gotoScene( "Scenes.LoginScene", "slideLeft", 400)
 
+
+
+
+-- Runtime Events
+--[[
+local function onEnterFrame( event )
+    
+    local eventType = event.type
+    
+end
+
+local function onSystemEvent( event )
+    
+    local eventType = event.type
+    
+    if eventType == "applicationSuspend" then
+        --
+    elseif eventType == "applicationResume" then
+        --
+    end
+end
+
+local function onOrientationEvent( event )
+    
+    local eventType = event.type
+    
+end
+
+
+
+Runtime:addEventListener("enterFrame", onEnterFrame)
+Runtime:addEventListener("system", onSystemEvent)
+Runtime:addEventListener("orientation", onOrientationEvent)
+--]]
 
