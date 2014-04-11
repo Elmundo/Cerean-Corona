e973131a81d75f30d0056bf524b99e65716b96bc
Baris YILMAZ <baris.yilmaz.1985@gmail.com>
2014-04-11 12:07:22 +0300

    BillImages are added.

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ require "ParameterConfig"
 -- Set default anchor point of project top-left
 display.setDefault( "anchorX", 0 )
 display.setDefault( "anchorY", 0 )
-display.setDefault("background", 1, 1, 1, 1)
+display.setDefault( "background", 1, 1, 1, 1)
 
 -- Extend native libs
 string.trim = function (str)
@@ -29,39 +29,3 @@ Logger:debug("main", "general", "ContentWidth: " .. display.contentWidth .. " Co
 Logger:debug("main", "general", "ContentWidth: " .. display.contentWidth .. " ContentHeight: " .. display.contentHeight)
 
 storyboard.gotoScene( "Scenes.LoginScene", "slideLeft", 400)
-
-
-
-
--- Runtime Events
---[[
-local function onEnterFrame( event )
-    
-    local eventType = event.type
-    
-end
-
-local function onSystemEvent( event )
-    
-    local eventType = event.type
-    
-    if eventType == "applicationSuspend" then
-        --
-    elseif eventType == "applicationResume" then
-        --
-    end
-end
-
-local function onOrientationEvent( event )
-    
-    local eventType = event.type
-    
-end
-
-
-
-Runtime:addEventListener("enterFrame", onEnterFrame)
-Runtime:addEventListener("system", onSystemEvent)
-Runtime:addEventListener("orientation", onOrientationEvent)
---]]
-
